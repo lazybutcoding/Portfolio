e9dbf731d38c7355a4ec249044d8dcac0c4b7629
lazybutcoding <anishpawar077@gmail.com>
2026-02-17 17:37:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({
+        behavior: "smooth"
+      });
+  });
+});
